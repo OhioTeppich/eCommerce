3ab0d5fbdda026109e9e37253d586ecc9aaaf564
Christian Maciosek <christian-maciosek@t-online.de>
2023-02-11 23:48:31 +0100

    AppBar, NavBar & App Route

---
+++ lib/config/app_router.dart
@@ -0,0 +1,39 @@
+import 'dart:developer';
+
+import 'package:ecommerce/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  static Route onGenerateRoute(RouteSettings settings) {
+    log('This is route: $settings');
+
+    switch (settings.name) {
+      case '/':
+        return HomeScreen.route();
+/*       case HomeScreen.routeName:
+        return HomeScreen.route(); */
+      case CartScreen.routeName:
+        return CartScreen.route();
+      case WishlistScreen.routeName:
+        return WishlistScreen.route();
+      case ProductScreen.routeName:
+        return CartScreen.route();
+      case CatalogScreen.routeName:
+        return CartScreen.route();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route _errorRoute() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: '/error'),
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Text('Error'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = '/';
+
+  const HomeScreen({super.key});
+
+  static Route route() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => const HomeScreen(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: CustomAppBar(title: 'Zero To Unicorn'),
+      bottomNavigationBar: CustomNavBar(),
+    );
+  }
+}
